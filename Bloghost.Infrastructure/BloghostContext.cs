54043349fe444fd677fa29ddae4a3db6983afd79
Daria Nesterovich <dashka.n00@gmail.com>
2019-12-08 19:17:16 +0300

    commit

---
+++ Bloghost.Infrastructure/BloghostContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloghost.Infrastructure
+{
+    public class BloghostContext : DbContext
+    {
+        public BloghostContext(DbContextOptions<BloghostContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
